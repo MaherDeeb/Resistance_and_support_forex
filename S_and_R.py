3826fb7126f6932f741d53174674a710e8ff4e61
Maher Deeb <maherdeebcv@gmail.com>
2019-12-07 00:15:02 +0100

    refactor the main functions

--- S_and_R.py
+++ S_and_R.py
@@ -23,73 +23,149 @@ def main(filename):
     dataframe = load_data(filename)
 
 
-# ==============================================================================
-# 4. inputs
-# 4.1 considered periods = number of days * 5 candles per day
-t = 60 * 5
-# 4.2 the minimum and the maximum size of the window that will explore the data
-ws_min, ws_max = 5, 75
-# 4.2 Initial values of the R and S
-R = defaultdict(list)
-S = defaultdict(list)
-tot_R_S = []
+if __name__ == '__main__':
+    # Inputs
+    # considered periods = number of days * 5 candles per day if the frequency is 4 hours
+    # number_of_candles = 60 * 5
+    # the minimum and the maximum size of the window that will explore the data
+    # minimum_window_size, maximum_window_size = 5, 75
+    #  Initial values of the resistances and supports
+    resistances = defaultdict(list)
+    supports = defaultdict(list)
+
+    # define the power dict
+    # p = {}
+    # initial value of maximum hold: we compare other resistances and supports values based on it
+    # max_h = 0
+    # define the accuracy in points: it is important to calculate when the price holds and when not
+    # accu = 0.0005
+
+    main()
+
+
+def get_single_top_price_in_window(dataframe, candle_i, window_size_i):
+    top = max(dataframe['high'][candle_i + 1:candle_i + window_size_i])
+    return top
+
+
+def get_single_low_price_in_window(dataframe, candle_i, window_size_i):
+    low = min(dataframe['low'][candle_i + 1:candle_i + window_size_i])
+    return low
+
+
 # ==============================================================================
 # 5. process:
-for ws in range(ws_min, ws_max + 1):
-    for t_i in range(l - t, l - ws + 2):
-        top = max(f['H'][t_i + 1:t_i + ws])
-        low = min(f['L'][t_i + 1:t_i + ws])
-        # be sure that the values are unique
-        if top not in R[ws]:
-            R[ws].append(top)
-            tot_R_S.append(top)
-        if low not in S[ws]:
-            S[ws].append(low)
-            tot_R_S.append(low)
+def extract_resistances_supports(number_of_rows: int,
+                                 number_of_candles: int,
+                                 minimum_window_size: int,
+                                 maximum_window_size: int,
+                                 dataframe):
+    resistances_supports_list = []
+
+    for window_size_i in range(minimum_window_size, maximum_window_size + 1):
+        for candle_i in range(number_of_rows - number_of_candles, number_of_rows - window_size_i + 2):
+            top = get_single_top_price_in_window(dataframe, candle_i, window_size_i)
+            low = get_single_low_price_in_window(dataframe, candle_i, window_size_i)
+            # be sure that the values are unique
+            if top not in resistances[window_size_i]:
+                resistances[window_size_i].append(top)
+                resistances_supports_list.append(top)
+            if low not in supports[window_size_i]:
+                supports[window_size_i].append(low)
+                resistances_supports_list.append(low)
+    return resistances_supports_list
+
+
 # ==============================================================================
 
 # ==============================================================================
-# 7. calculate the strength of the S or R
+# 7. calculate the strength of the supports or resistances
 # 7.1 remove repeated values
-s_tot_R_S = list(set(tot_R_S))
-# define the power dict
-p = {}
-# initial value of maximum hold: we compare other R and S values based on it
-max_h = 0
-# define the accuracy in points: it is important to calculate when the price holds and when not
-accu = 0.0005
-# 7.2 apply the method
-for j in range(len(s_tot_R_S)):
-    vis = 0  # how many would be the R or S visited
-    hol = 0  # how many the R or S visited holds
-    for i in range(1, t):
-        if f['L'][l - i] - accu <= s_tot_R_S[j] <= f['H'][
-            l - i] + accu:  # R or S is visited if it is incide the highest and lowest price of the candle
-            vis += 1
-            if abs(f['H'][l - i] - s_tot_R_S[j]) < accu and max(f['C'][l - i:l - i + 2]) < s_tot_R_S[
-                j]:  # R holds if the highest price stops at R and the next candle close under the R
-                hol += 1
-            if abs(f['L'][l - i] - s_tot_R_S[j]) < accu and min(f['C'][l - i:l - i + 2]) > s_tot_R_S[
-                j]:  # S holds if the lowest price stops at S and the next candle close under the S
-                hol += 1
-    max_h = max(max_h, hol)
-    p.update({s_tot_R_S[j]: [vis, hol, hol * 100 / vis]})
-# 7.3 calculate the strength % based on the maximum number of hold (compare R and S to each other)
-# low value means that R or S are not enough tested in the given period or it was not holding enought (weak)
-power_S_R = []
-filter_R_S = 0
-for j in range(len(s_tot_R_S)):
-    p[s_tot_R_S[j]][2] = p[s_tot_R_S[j]][1] * 100 / max_h
-    if p[s_tot_R_S[j]][2] > filter_R_S:
-        power_S_R.append(p[s_tot_R_S[j]][2])
-    else:
-        power_S_R.append(0)
+
+def lowest_highest_candle_price(dataframe, number_of_rows, candle_i, tolerance):
+    lowest_candle_price = dataframe["low"][number_of_rows - candle_i] - tolerance
+    highest_candle_price = dataframe["high"][number_of_rows - candle_i] + tolerance
+    return lowest_candle_price, highest_candle_price
+
+
+def resistances_hold(hold_price, price_i, dataframe, number_of_rows, candle_i, tolerance):
+    # resistances holds if the highest price stops at those
+    # resistances and the next candle close under those resistances
+    if abs(dataframe["high"][number_of_rows - candle_i] - price_i) < tolerance and max(
+            dataframe["close"][number_of_rows - candle_i:number_of_rows - candle_i + 2]) < \
+            price_i:
+        hold_price += 1
+    return hold_price
+
+
+def supports_hold(hold_price, price_i, dataframe, number_of_rows, candle_i, tolerance):
+    # supports holds if the lowest price stops at supports and the next candle close under the supports
+    if abs(dataframe["low"][number_of_rows - candle_i] - price_i) < tolerance and min(
+            dataframe["close"][number_of_rows - candle_i:number_of_rows - candle_i + 2]) > price_i:
+        hold_price += 1
+    return hold_price
+
+
+def calculate_strength(resistances_supports_list,
+                       number_of_candles,
+                       number_of_rows,
+                       dataframe,
+                       tolerance):
+    unique_resistances_supports_list = list(set(resistances_supports_list))
+
+    maximum_hold_observed = 0
+    power_dict = {}
+
+    for price_i in unique_resistances_supports_list:
+
+        # how many times would be the resistances or supports visited independent of they are exceeded or price reversed
+        visited_price = 0
+        # how many times the resistances or supports that are visited visited hold
+        # i.e. price reverse after reaching those prices
+        hold_price = 0  # how many the resistances or supports visited holds
+
+        for candle_i in range(1, number_of_candles):
+            # resistances or supports is visited if it is inside the highest and lowest price of the candle
+            lowest_candle_price, highest_candle_price = lowest_highest_candle_price(dataframe,
+                                                                                    number_of_rows,
+                                                                                    candle_i,
+                                                                                    tolerance)
+            if lowest_candle_price <= price_i <= highest_candle_price:
+                visited_price += 1
+                # resistances holds if the highest price stops at those
+                # resistances and the next candle close under those resistances
+
+                hold_price = resistances_hold(hold_price, price_i, dataframe, number_of_rows, candle_i, tolerance)
+                hold_price = supports_hold(hold_price, price_i, dataframe, number_of_rows, candle_i, tolerance)
+
+        maximum_hold_observed = max(maximum_hold_observed, hold_price)
+
+        power_dict.update({price_i: [visited_price, hold_price, hold_price * 100 / visited_price]})
+
+    return power_dict, maximum_hold_observed
+
+    # 7.3 calculate the strength % based on the maximum number of hold (compare resistances and supports to each other)
+    # low value means that resistances or supports are not enough tested in the given period or it was not holding enought (weak)
+
+
+def scale_power(power_dict, unique_resistances_supports_list, maximum_hold_observed):
+    power_resistances_supports_list = []
+    power_threshold = 0
+    for price_i in unique_resistances_supports_list:
+        power_dict[price_i][2] = power_dict[price_i][1] * 100 / maximum_hold_observed
+        if power_dict[price_i][2] > power_threshold:
+            power_resistances_supports_list.append(power_dict[price_i][2])
+        else:
+            power_resistances_supports_list.append(0)
+    return power_dict
+
+
 # ==============================================================================
-# 8. For visualization purposes only the R and S values close to the current price will be presented
+# 8. For visualization purposes only the resistances and supports values close to the current price will be presented
 # 8.1 current price is the close of the last candle
-cur = f['C'][len(f['C']) - 1]
+cur = dataframe["close"][len(dataframe["close"]) - 1]
 # 8.2 define the range of presenting the data based on the range of price movement which is the difference between the highest and lowest value of each candle
-df_h_l = f['H'] - f['L']
+df_h_l = dataframe["high"] - dataframe["low"]
 # 8.2.1 Histogram of the price movement can be checked to understand the results.
 # plt.hist(df_h_l)
 # 8.3 Printing the results
@@ -99,15 +175,15 @@ ax = fig.add_subplot(111)
 ax.plot([0, 100], [cur, cur], label='current price')
 ax.set_xlim(0, 100)
 ax.barh(s_tot_R_S, power_S_R, align='center',
-        color='green', ecolor='black', height=0.0001, label='R or S')
+        color='green', ecolor='black', height=0.0001, label='resistances or supports')
 ax.set_ylim(cur - df_h_l.mean(), cur + df_h_l.mean())
-ax.set_xlabel('Strength of R and S %')
+ax.set_xlabel('Strength of resistances and supports %')
 ax.set_ylabel('Price')
 ax.set_title('Current price and the closest support and resistances and their strengths')
 ax.grid(True)
 ax.legend()
 # ==============================================================================
-# 9. Apply k-mean to cluster the R and S
+# 9. Apply k-mean to cluster the resistances and supports
 # 9.1 choose K
 K = 100
 init_arr = []
